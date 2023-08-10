167ec330181c8a6d6d8f204de5237356b62fd232
Fujoe <joesanni7@gmail.com>
2023-08-10 16:35:22 -0700

    A C program that prints an output and follows it with a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
+ * main - a C program that prints exactly an output, and follows it with a new line
  *
- * This function prints a message to the standard output.
- *
- * Return: Always 0 (indicating success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
