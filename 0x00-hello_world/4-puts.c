ff637ee73ec66dbf993c5c9e4d653b9562362328
Fujoe <joesanni7@gmail.com>
2023-08-10 16:25:44 -0700

    A C program that prints a name, and enters a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a message to the standard output.
+ *
+ * Return: Always 0 (indicating success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
