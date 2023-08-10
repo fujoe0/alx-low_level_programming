77447f502c152036b629cbfdecfd7aa8e6ebd641
Fujoe <joesanni7@gmail.com>
2023-08-10 16:53:26 -0700

    a C program that prints an output and follows it by exactly one line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a C program that prints exactly an output, 
- *and follows it with a new line
- *
- * Return: 0 (Success)
- */
+* main - a C program that prints exactly an output, 
+*and follows it with a new line
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
