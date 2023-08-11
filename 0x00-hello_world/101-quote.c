9c4b00187ad4e5547e9cc551f4bbe100f60708b2
Fujoe <joesanni7@gmail.com>
2023-08-10 17:38:00 -0700

    A C program that prints input, and follows it with a new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful"
- * followed by a new line, to the standard error.
- * 
+ * main - Prints exactly "and that piece of art is useful" - Dora Korpar,
+ *        2015-10-19, followed by a new line, to the standard error.
+ *
  * Return: Always 1.
  */
 int main(void)
