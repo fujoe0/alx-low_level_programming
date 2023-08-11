94b18fdf4daac0b4cab6b98f8f3723a4e6c31c08
Fujoe <joesanni7@gmail.com>
2023-08-11 16:10:15 -0700

    This program prints the alphabet in lowercase, with the exception of letter q and letter e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 {
 char x;
 
-for (x = 'a'; y <= 'z'; x++)
-if (x != 'e' && x != 'q')
-putchar(x);
+for (c = 'a'; y <= 'z'; c++)
+if (c != 'e' && c != 'q')
+putchar(c);
 putchar('\n');
 return (0);
 }
