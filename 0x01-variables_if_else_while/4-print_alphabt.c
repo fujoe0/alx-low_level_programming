7384e3ac33b52688559a3b7343a74762e61b5108
Fujoe <joesanni7@gmail.com>
2023-08-11 16:08:25 -0700

    This program prints the alphabet in lowercase, with the exception of letter q and letter e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A  program that prints the alphabet in lowercase
+ * with the exception of letter 'q' and letter 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+
+for (x = 'a'; y <= 'z'; x++)
+if (x != 'e' && x != 'q')
+putchar(x);
+putchar('\n');
+return (0);
+}
+
