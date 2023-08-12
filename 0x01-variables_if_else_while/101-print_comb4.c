a4316d56e44ef32b344f5eae74f556d0578aeed1
Fujoe <joesanni7@gmail.com>
2023-08-11 17:59:16 -0700

    This program prints all possible combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,28 @@
  * Return: Always 0.
  */
 int main(void)
-        {
-        int x;
-        int y;
-        int z;
+	{
+	int x;
+	int y;
+	int z;
 
-        for (x = 48; x <= 55; x++)
-        {
-        for (y = x + 1; y <= 56; y++)
-        {
-                for (z = y + 1; z <= 57; z++)
-                {
-                putchar(x);
-                putchar(y);
-                putchar(z);
-                if (x == 55 && y == 56 && z == 57)
-                        continue;
-                putchar(',');
-                putchar(' ');
-                }
-        }
-        }
-        putchar('\n');
-        return (0);
-        }
+	for (x = 48; x <= 55; x++)
+	{
+	for (y = x + 1; y <= 56; y++)
+	{
+		for (z = y + 1; z <= 57; z++)
+		{
+		putchar(x);
+		putchar(y);
+		putchar(z);
+		if (x == 55 && y == 56 && z == 57)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+	}
+
