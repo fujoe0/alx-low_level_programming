3774fcec3f3ae9f9a7766fae3247f030c5519650
Fujoe <joesanni7@gmail.com>
2023-08-15 15:35:51 -0700

    This program prints _putchar, and follows it with a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
-#include "main.h"
-
-/**
- * defines a new function in the
- * <main.h> header file
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_tablet(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_tablet(int n);
