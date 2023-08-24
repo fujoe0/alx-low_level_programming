9dcfd9338c78c775fa2deb742dccea2bd3877fe0
Fujoe <joesanni7@gmail.com>
2023-08-24 09:25:31 -0700

    This function prints an integer

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char word[8] = "putchar";
+        int i;
+
+        for (i = 0; i < 8; i++)
+                putchar(word[i]);
+        putchar('\n');
+
+        return (0);
+}
