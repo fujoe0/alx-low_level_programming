fb6e529a85ad04403182f13761c020fe8f90b05a
Fujoe <joesanni7@gmail.com>
2023-08-28 07:17:24 -0700

    This function fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,8 @@
 
 /**
  * * _memset - Entry point
- * @s: A pointer to the destination memory area where the constant byte will be stored.
+ * @s: A pointer to the destination memory area 
+ * where the constant byte will be stored.
  * @b: The constant byte that will be stored in the memory area.
  * @n: The number of bytes to fill with the constant byte.
  * Return: Always 0 (Success)
