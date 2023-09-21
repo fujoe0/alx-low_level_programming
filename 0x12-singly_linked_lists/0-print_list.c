247da9c6b8396dc3a543a95302ef6cb76d5542fb
fujoe0 <joesanni7@gmail.com>
2023-09-21 11:17:44 -0700

    Attempt at resolving code errord

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t A;
+	size_t nelem;
 
-	A = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		A++;
+		nelem++;
 	}
-	return (A);
+	return (nelem);
 }
